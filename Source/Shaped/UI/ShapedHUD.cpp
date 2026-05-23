988a1af86714867ffcd886e5aa408ff69d2e6ff3
Martin Bakus <136487973+Retr-1@users.noreply.github.com>
2026-05-23 12:54:57 +0200

    made factory map

--- Source/Shaped/UI/ShapedHUD.cpp
+++ Source/Shaped/UI/ShapedHUD.cpp
@@ -21,40 +21,6 @@ void AShapedHUD::DrawHUD()
 	}
 
 	RefreshStatusWidgetBinding();
-
-	const FVector2D ScreenCenter(Canvas->ClipX * 0.5f, Canvas->ClipY * 0.5f);
-
-	DrawLine(
-		ScreenCenter.X - CursorGap - CursorHalfSize,
-		ScreenCenter.Y,
-		ScreenCenter.X - CursorGap,
-		ScreenCenter.Y,
-		CursorColor,
-		CursorThickness);
-
-	DrawLine(
-		ScreenCenter.X + CursorGap,
-		ScreenCenter.Y,
-		ScreenCenter.X + CursorGap + CursorHalfSize,
-		ScreenCenter.Y,
-		CursorColor,
-		CursorThickness);
-
-	DrawLine(
-		ScreenCenter.X,
-		ScreenCenter.Y - CursorGap - CursorHalfSize,
-		ScreenCenter.X,
-		ScreenCenter.Y - CursorGap,
-		CursorColor,
-		CursorThickness);
-
-	DrawLine(
-		ScreenCenter.X,
-		ScreenCenter.Y + CursorGap,
-		ScreenCenter.X,
-		ScreenCenter.Y + CursorGap + CursorHalfSize,
-		CursorColor,
-		CursorThickness);
 }
 
 void AShapedHUD::InitializeStatusWidget()
